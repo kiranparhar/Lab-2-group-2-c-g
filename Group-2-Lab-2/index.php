acc2b50d7e43aea682c01d96eea15dc7f4db3ce6
mashhood895 <55720513+mashhood895@users.noreply.github.com>
2020-10-19 23:56:39 -0400

    update index.php

--- Group-2-Lab-2/index.php
+++ Group-2-Lab-2/index.php
@@ -10,7 +10,7 @@ if ($conn -> connect_errno) {
   echo "Failed to connect to MySQL: " . $conn -> connect_error;
   exit();
 }
-	
+  
  ?>
 <!DOCTYPE html>
 <html>
@@ -22,12 +22,12 @@ if ($conn -> connect_errno) {
 
 <?php 
 if(isset($_POST['submit'])) {
-	$cname= $_POST['name'];
-	$ciso= $_POST['iso'];
-	$cid= $_POST['cid'];
-	$sql3 = "INSERT INTO `country`(`Name`, `ISO`, `Continent_ID`) VALUES
+  $cname= $_POST['name'];
+  $ciso= $_POST['iso'];
+  $cid= $_POST['cid'];
+  $sql3 = "INSERT INTO `country`(`Name`, `ISO`, `Continent_ID`) VALUES
      ('".$cname."', '".$ciso."', '".$cid."')";
-	 $conn->query($sql3);
+   $conn->query($sql3);
 }
 
 if(isset($_GET['Country']) && isset($_GET['name'])) {
@@ -35,37 +35,37 @@ $Countryid=$_GET['Country'];
 $Countryname=$_GET['name']; ?>
 <div class="container">
     <h2 class="pageTitle">Country <?php echo $_GET['name']?></h2>
-	  <?php 
+    <?php 
   $i=1;
   $sql = "SELECT `ID`, `Name`, `ISO`, `description`, `Continent_ID`,(SELECT `Image` FROM `countryflag` WHERE `Country_ID`=country.ID limit 1) as flag FROM `country` WHERE `Continent_ID`=".$_GET['Country'];
-	 if ($result = $conn -> query($sql)) {
-	  while ($row = $result -> fetch_row()) { 
-	  ?>
-	 <p class="listp"><b><?php echo $i; ?></b>  <?php echo $row[1]; ?> ISO(<?php echo $row[2]; ?>)&nbsp;&nbsp;&nbsp;
-	 <?php if($row[5]) {?>
-	 <img class="flagimg" src="data:image/png;base64,<?php echo $row[5]?>"/>
-	 <?php } else {?>
-	  <img class="flagimg" src="images/placeholder.png"/>
-	 <?php }?>
-	 </p>
-	 <h4 class="h4title" >Gallery :- </h4>
-	 <div class="gallerydiv">
-	 <?php 
-		  $sql2 = "SELECT `ID`, `Country_ID`, `Image_Path`, `Name`, `Description` FROM `countryimagesgallery` WHERE `Country_ID`=".$row[0];
-		  if ($result2 = $conn -> query($sql2)) {
-			while ($row2 = $result2 -> fetch_row()) { ?>
-			 <img class="galleryimg"  alt="<?php echo $row2[3]?>" src="<?php echo $row2[2]?>"/>
-	  <?php  } } ?>
-	 </div>
-	 <div class="btngroup">
-	 <a href="edit.php?Countryid=<?php echo $row[0]; ?>&name=<?php echo $row[1]; ?>" class="btn">Edit</a>
-	 <a href="javascript:void(0)" rel="<?php echo $row[0]; ?>"  table="country" class="btn deletedata">Delete</a>
-	 </div>
-	 <hr>
-	<?php 
-	  $i++;
-	  }
-	}
+   if ($result = $conn -> query($sql)) {
+    while ($row = $result -> fetch_row()) { 
+    ?>
+   <p class="listp"><b><?php echo $i; ?></b>  <?php echo $row[1]; ?> ISO(<?php echo $row[2]; ?>)&nbsp;&nbsp;&nbsp;
+   <?php if($row[5]) {?>
+   <img class="flagimg" src="data:image/png;base64,<?php echo $row[5]?>"/>
+   <?php } else {?>
+    <img class="flagimg" src="images/placeholder.png"/>
+   <?php }?>
+   </p>
+   <h4 class="h4title" >Gallery :- </h4>
+   <div class="gallerydiv">
+   <?php 
+      $sql2 = "SELECT `ID`, `Country_ID`, `Image_Path`, `Name`, `Description` FROM `countryimagesgallery` WHERE `Country_ID`=".$row[0];
+      if ($result2 = $conn -> query($sql2)) {
+      while ($row2 = $result2 -> fetch_row()) { ?>
+       <img class="galleryimg"  alt="<?php echo $row2[3]?>" src="<?php echo $row2[2]?>"/>
+    <?php  } } ?>
+   </div>
+   <div class="btngroup">
+   <a href="edit.php?Countryid=<?php echo $row[0]; ?>&name=<?php echo $row[1]; ?>" class="btn">Edit</a>
+   <a href="javascript:void(0)" rel="<?php echo $row[0]; ?>"  table="country" class="btn deletedata">Delete</a>
+   </div>
+   <hr>
+  <?php 
+    $i++;
+    }
+  }
   ?>
   <form  class="add" method="POST" action="?Country=<?php echo $Countryid; ?>&name=<?php echo $Countryname; ?>">
   <h2>Add Country</h2>
@@ -82,7 +82,7 @@ $Countryname=$_GET['name']; ?>
 <?php } else { ?>
 <div class="container">
     <h2 class="pageTitle">Continent</h2>
-	<table>
+  <table>
   <tr>
     <th>S.No</th>
     <th>ISO</th>
@@ -100,7 +100,7 @@ $Countryname=$_GET['name']; ?>
     <td><a href="?Country=<?php echo $row[0]; ?>&name=<?php echo $row[2]; ?>"><?php echo $row[2]; ?></a></td>
   </tr>
 <?php 
-	$i++;
+  $i++;
    }
 }
  ?> 
